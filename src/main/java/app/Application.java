74bab3f126dc95b29c4099ac0f44dc0f308a3399
kak7NotSupported~ <kaka0monstr@gmail.com>
2024-02-14 19:31:22 +0300

    fixed

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,24 @@
+package app;
+
+import app.config.AppConfig;
+import app.model.AnimalsCage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        for (int i = 0; i < 5; i++) {
+            AnimalsCage bean = applicationContext.getBean(AnimalsCage.class);
+            bean.whatAnimalSay();
+        }
+    }
+
+}
